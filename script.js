4a4ae7baee651d36ef43e59b5b3ec8af7dc019b1
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-03-22 17:57:33 -0400

    Set up objects and html sections

---
+++ script.js
@@ -0,0 +1,15 @@
+const gameBoard = (() => {
+  let array = [];
+
+  return array;
+})();
+
+const Player = (mark) => {
+    const mark = mark;
+
+    return { mark };
+}
+
+const displayController = (() => {
+
+})();
