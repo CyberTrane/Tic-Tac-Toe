c667efb0913f6324ceb67abca2eef15c0a96fcef
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2024-06-07 17:29:52 -0400

    added function for player to input their mark on the board

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const gameBoard = (() => {
   // let row;
   // let col;
-  const board = [[1, 2, 3],
-                 [4, 5, 6],
-                 [7, 8, 9]];
+  const board = [['1', '2', '3'],
+                 ['4', '5', '6'],
+                 ['7', '8', '9']];
   
   // return {
   //   // logStuff: () => {console.log('stuff')}
@@ -15,6 +15,7 @@ const gameBoard = (() => {
   return {
     // row: row,
     // col: col,
+    board: board,
     render: () => {
       for (let i of board) {
         const string = i.join(' ');
@@ -36,7 +37,24 @@ function Player(name, mark) {
     name: name,
     mark: mark,
     inputMark: (mark) => {
-      let userInput = Number(prompt('Each number corresponds to a free space. Please enter a number to make your move:'));
+      let userInput = prompt(name + ', each number corresponds to a free space. Please enter a number to make your move:');
+      let x = true;
+      const board = gameBoard.board;
+      
+      while (x) {
+        for (let i = 0; i < board.length; i++) {
+          for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === userInput) {
+              gameBoard.updateBoard(i, j, mark);
+              x = false;
+              break;
+            }
+          }
+        }
+        if (x) {
+          userInput = prompt('That space is not available, please enter again:');
+        }
+      }
     }
   }
 
@@ -54,16 +72,18 @@ const gameController = (() => {
     p1Mark = prompt('Error: please enter either X or O:').toUpperCase();
   }
 
-  const p2Name = prompt('Player 2, please enter your name:');
-  let p2Mark;
+  // const p2Name = prompt('Player 2, please enter your name:');
+  // let p2Mark;
 
-  if (p1Mark === 'X') {
-    p2Mark = 'O';
-  } else {
-    p2Mark = 'X';
-  }
+  // if (p1Mark === 'X') {
+  //   p2Mark = 'O';
+  // } else {
+  //   p2Mark = 'X';
+  // }
 
   const player1 = Player(p1Name, p1Mark);
-  const player2 = Player(p2Name, p2Mark);
+  // const player2 = Player(p2Name, p2Mark);
+
+  player1.inputMark(p1Mark);
 
 })();
